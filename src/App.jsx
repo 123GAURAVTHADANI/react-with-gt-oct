1321d78c45480d480d5f3845103b6c53c9003bd2
123GAURAVTHADANI <garv.thadani@gmail.com>
2026-07-19 21:43:46 +0530

    Initialize code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 // functional component!!
 function App() {
   // hook
   const [counter, setCounter] = useState(0);
   const [theme, setTheme] = useState("light");
+  const [apiData, setApiData] = useState([]);
   const handleBtnClick = () => {
     setCounter(counter + 1);
   };
+
   const handleToogleBtn = () => {
     setTheme(theme == "light" ? "dark" : "light");
   };
+
   console.log("Inside App!!!", theme);
+
+  // updating!!!
+  useEffect(() => {
+    async function handleDataFetch() {
+      const response = await fetch("https://dummyjson.com/products");
+      const json_data = await response.json();
+      setApiData(json_data);
+    }
+    handleDataFetch();
+  }, []);
+
   return (
     <div
       style={{
@@ -23,13 +37,20 @@ function App() {
       }}
     >
       <Header counter={counter} />
-      <h1>Main Page</h1>
+      <h1 className="header">Main Page</h1>
       <button onClick={handleBtnClick}>Increment</button>
       <button onClick={handleToogleBtn}>Toggle</button>
       <h4>{counter}</h4>
+      <ul>
+        {apiData?.products?.map((item) => {
+          return <li>{item.title}</li>;
+        })}
+      </ul>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+// rendering // mounting !!!
